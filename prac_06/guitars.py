38c038bf70f653a24a4a54e819eeb983afaaf048
TrandGram <206093236+TrandGram@users.noreply.github.com>
2025-11-01 13:06:32 +0800

    add this to prac_06

---
+++ prac_06/guitars.py
@@ -0,0 +1,29 @@
+"""CP1404/CP5632 Practical - Client program to manage Guitars."""
+
+from guitar import Guitar
+
+
+def main():
+    """Collect and display user's guitars."""
+    guitars = []
+    print("My guitars!")
+    name = input("Name: ")
+    while name != "":
+        year = int(input("Year: "))
+        cost = float(input("Cost: $"))
+        guitar = Guitar(name, year, cost)
+        guitars.append(guitar)
+        print(f"{guitar} added.\n")
+        name = input("Name: ")
+
+    if guitars:
+        print("\nThese are my guitars:")
+        for i, guitar in enumerate(guitars, 1):
+            vintage_string = " (vintage)" if guitar.is_vintage() else ""
+            print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}")
+    else:
+        print("No guitars entered.")
+
+
+if __name__ == "__main__":
+    main()
---
+++ prac_06/languages.py
@@ -0,0 +1,23 @@
+"""
+CP1404 Practical
+"""
+
+from prac_06.programming_language import ProgrammingLanguage
+
+def main():
+    """Test the ProgrammingLanguage class."""
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+
+    languages = [python, ruby, visual_basic]
+
+    print(python)
+    print("\nThe dynamically typed languages are:")
+    for language in languages:
+        if language.is_dynamic():
+            print(language.name)
+
+
+if __name__ == "__main__":
+    main()
